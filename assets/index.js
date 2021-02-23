06afa6124cefa5c5864a10c1a4ceb05777d69623
Wendy Hernandez <hwendy750@gmail.com>
2021-02-22 21:36:55 -0600

    Funcionalidad finalizada

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,11 @@
 new Vue ({
     el: "#app",
     data:{
-        money:{}
+        money: {},
+        amount: 0,
+        from: 'EUR',
+        to: 'USD',
+        result: 0
     },
     mounted(){
         this.getMoney();
@@ -9,6 +13,12 @@ new Vue ({
     computed: {
         formatMoney(){
             return Object.values(this.money);
+        },
+        calculateResult(){
+            return(Number(this.amount) * this.result).toFixed(3);
+        },
+        disabled(){
+            return this.amount === 0 || !this.amount;
         }
     },
     methods: {
@@ -22,10 +32,26 @@ new Vue ({
 
             axios.get('https://free.currconv.com/api/v7/currencies?apiKey=do-not-use-this-key')
             .then( response => {
-            this.money = response.data.results;
-            localStorage.setItem('money' , JSON.stringify(response.data.results));
-            //console.log(response)
-        });
+                this.money = response.data.results;
+                localStorage.setItem('money' , JSON.stringify(response.data.results));
+            });
+        },
+        convertMoney(){
+            const search = `${this.from}_${this.to}`;
+            axios.get(`https://free.currconv.com/api/v7/convert?q=${search}&apiKey=do-not-use-this-key`)
+            .then((response) => {
+                console.log(response);
+                this.result = response.data.results[search].val;
+            });
         }
-    },  
+    },
+    
+    watch:{
+        from(){
+            this.result = 0;
+        },
+        to(){
+            this.result = 0;
+        }
+    }
 });
